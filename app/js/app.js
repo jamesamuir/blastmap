5ab38d3d519e95badf139b1f74b5a0545168606f
jmuir <jamesamuir@gmail.com>
2013-04-08 14:10:44 -0400

    modal links working

--- app/js/app.js
+++ app/js/app.js
@@ -4,9 +4,7 @@
 // Declare app level module which depends on filters, and services
 var app = angular.module('myApp', ["google-maps", "bootstrap-module"]).
     config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: HomeCtrl});
     $routeProvider.when('/map', {templateUrl: 'partials/map.html', controller: MapCtrl});
-        $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: ContactCtrl});
     $routeProvider.otherwise({redirectTo: '/map'});
 }]);
 
